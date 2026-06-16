7c2735e0b330518e28843a2e8ff5c5a290bc271d
Ulpian Cesana <3241504+ucesana@users.noreply.github.com>
2026-06-16 18:15:04 +1000

    Added Identity store.
    # Conflicts:
    #	app/store/store.ts

--- app/store/store.ts
+++ app/store/store.ts
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import sessionReducer from "./identitySlice";
+import identityReducer from "./identitySlice";
 
 export const store = configureStore({
   reducer: {
-    session: sessionReducer,
+    session: identityReducer,
   },
 });
 
